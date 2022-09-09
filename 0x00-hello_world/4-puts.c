5c861b10f72c6357ac4fa951fe88362b426be95f
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-09 14:18:46 -0700

    4-puts.c content with solving the comment problem

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/** The function main prints the text followed by a new line.*/
+/**
+ * main - print a text
+ * void : nothing in
+ * The function main prints the text followed by a new line.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
