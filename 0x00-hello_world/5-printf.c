a20fdd3d0d574463ced0f60fe49321d4efc828ef
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-09 14:35:24 -0700

    5-printf.c c program file created with content

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a text
+ * void : nothing in
+ * The function main prints the text followed by a new line using printf.
+ * and program should compile without warning when using the -Wall gcc option
+ * Return: 0 at the end
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
