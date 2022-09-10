b7178521aefad3aaffda1f34bdab154e5557f810
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-09 23:48:34 -0700

    I delete emacs backup files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* The function main prints the text followed by a new line.*/
+/** The function main prints the text followed by a new line.*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
