84ced32e46a41c165bbd81169a122b34e3fbf27a
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-12 05:17:31 -0700

    4-print_alphabts.c c file created with content

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char lw , e='e', q='q';
+
+	for (lw='a'; lw <= 'z'; lw++)
+	{
+		if (lw != e && lw != q)
+		putchar(lw);
+	}
+	putchar('\n');
+	return (0);
+}
