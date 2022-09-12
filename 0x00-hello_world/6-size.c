d6d06b6fc25f2dae9e070a9fab74452188e90282
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-12 02:04:59 -0700

    send 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print a text
- * void : nothing in
+11;rgb:0000/0000/0000* void : nothing in
  * The function main prints the text followed by a new line using printf.
  * and program should compile without warning when using the -Wall gcc option
  * Return: 0 at the end
