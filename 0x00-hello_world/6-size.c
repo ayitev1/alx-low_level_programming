82305d65f1d1a0f05601994e4b3fffa9142cc1a5
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-11 18:47:25 -0700

    6-size.c content changed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - print a text
+ * main - Entry point
  * void : nothing in
- * The function main prints the text followed by a new line using printf.
- * and program should compile without warning when using the -Wall gcc option
+ *  
  * Return: 0 at the end
  */
 
@@ -15,10 +14,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld bytes(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
