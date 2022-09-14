dffdd65fc03b8b147877533063ebff9d16cce924
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-13 19:57:05 -0700

    file main.h created and add to the code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 /** 
  * main - Entry point
  * 
  * this function is to print _putchar followed by a newline
- * Return: Always(0) - Success
+ * Return: 0 Always - Success
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(word[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
 
