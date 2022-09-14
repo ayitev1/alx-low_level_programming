f2dbaa8dd7f7c312a254518c781a5c2534a2dd0a
ayitev1 <firmin.hounouadossi@gmail.com>
2022-09-13 20:17:26 -0700

    1-alphabet.c c program file created with content

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * this function is to print alphabet in lowercase followed by a new line
+ * Return: 0 Always - Success
+ */
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
