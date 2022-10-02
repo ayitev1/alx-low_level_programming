bdc6dd9d7ad22b841c57ce639d0e8fa21d6acbb8
ayitev1 <firmin.hounouadossi@gmail.com>
2022-10-02 10:51:14 -0700

    6-abs.c c file with content updated by removing the implicit declaration in the function abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,16 +3,16 @@
 
 /**
  * _abs - Entry point
- * @x: parameter which absolute value will be given
+ *
  * this function is to compute the absolute value of an integer.
  * Return: 0 Always - Success
  */
 
-int _abs(int x)
+int _abs(int)
 {
 	int n;
 
-	n = abs(x);
+	n = abs(int);
 	_putchar('n');
 	return (0);
 }
