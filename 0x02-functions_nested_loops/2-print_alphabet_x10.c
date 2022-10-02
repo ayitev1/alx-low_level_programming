d854225068d119165d9386c7c8239168636206d0
ayitev1 <firmin.hounouadossi@gmail.com>
2022-10-01 20:53:31 -0700

    2-print_alphabet_x10.c with betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * this function is to print alphabet in lowercase followed by a new line
  * ten times
@@ -11,6 +11,7 @@ void print_alphabet_x10(void)
 {
 	char i;
 	int nb;
+	
 	for (nb = 0; nb < 10; nb++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
