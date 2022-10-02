80e0883102757ad4b36909783844220bdc4e3bd1
ayitev1 <firmin.hounouadossi@gmail.com>
2022-10-02 09:02:51 -0700

    4-isalpha.c c file created with content

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: a parameter to check for lowercase
+ * this function is to check for lowercase character
+ * Return: 0 Always - Success
+ */
+int _isalpha(int c);
+{
+	char ch;
+	int i;
+
+	for (ch = 'a' || ch = 'A'; ch <= 'z' || ch <= 'Z'; ch++)
+	{
+		if (ch == c)
+			i = 1;
+	}
+
+	return (i);
+}
