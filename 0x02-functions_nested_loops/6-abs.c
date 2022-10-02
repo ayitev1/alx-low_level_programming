c675df11e02b5d35a65dd80d9f4239764b49d1f4
ayitev1 <firmin.hounouadossi@gmail.com>
2022-10-02 10:47:45 -0700

    6-abs.c c file created with content

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @x: parameter which absolute value will be given
+ * this function is to compute the absolute value of an integer.
+ * Return: 0 Always - Success
+ */
+
+int _abs(int x)
+{
+	int n;
+
+	n = abs(x);
+	_putchar('n');
+	return (0);
+}
